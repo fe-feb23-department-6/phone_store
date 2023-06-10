698cb4f25ae3f1bc20197311dd0f9e2f12859462
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-06-10 03:37:20 +0300

    added basic header markup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { FC } from 'react';
+import './header.scss';
+import NiceGadgetsLogo from '../../img/Nice Gadgets.png';
+import Favourite from '../../img/icons/favourite.svg';
+import Basket from '../../img/icons/shopping_bag.svg';
 import { NavLink } from 'react-router-dom';
+import cn from 'classnames';
 
-export const Header = () => (
-  <nav>
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/phones">Phones</NavLink>
-    <NavLink to="/tablets">Tablets</NavLink>
-    <NavLink to="/accessories">Accessories</NavLink>
-  </nav>
-);
+export const Header: FC = () => {
+  return (
+    <div className="header">
+      <a href="#home" className="header__logo">
+        <img
+          src={NiceGadgetsLogo}
+          alt="logo"
+        />
+      </a>
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                cn('nav__link', { 'is-active': isActive })
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+
+          <li className="nav__item">
+            <a className="nav__link" href="#phones">
+              Phones
+            </a>
+          </li>
+
+          <li className="nav__item">
+            <a className="nav__link" href="#tablets">
+              Tablets
+            </a>
+          </li>
+
+          <li className="nav__item">
+            <a className="nav__link" href="#accessories">
+              Accessories
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="header__actions">
+        <a href="#favorites" className="action">
+          <img
+            src={Favourite}
+            alt="favourite"
+            className="action__image"
+          />
+        </a>
+
+        <a href="#basket" className="action">
+          <img
+            src={Basket}
+            alt="basket"
+            className="action__image"
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
