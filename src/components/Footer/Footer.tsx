b7b587ed5b9265683050a80718dfc308a1f598c5
Alisa Vinnyk <alisavinnyk@gmail.com>
2023-06-12 13:13:50 +0100

    added footer component and navigation link component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { NavigationLink } from '../NavigationLink/NavigationLink';
 
-export const Footer = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="#">Github</Link>
-      </li>
-      <li>
-        <Link to="#">Contacts</Link>
-      </li>
-      <li>
-        <Link to="#">Rights</Link>
-      </li>
-    </ul>
-  </nav>
+import './Footer.scss';
+import Logo from '../../img/nice_gadgets_logo.png';
+import BackToTop from '../../img/back_to_top.svg';
+
+export const Footer: React.FC = () => (
+  <footer className="footer">
+    <div className="footer__content">
+      <Link className="footer__logo footer__link" to="index.html">
+        <img src={Logo} alt="Nice Gadgets logo" />
+      </Link>
+
+      <nav className="footer-nav">
+        <ul className="footer-nav__list">
+          <li className="footer-nav__item">
+            <NavigationLink
+              className="footer-nav__link"
+              to="/"
+              title="Gadgets"
+            />
+          </li>
+          <li className="footer-nav__item">
+            <NavigationLink
+              className="footer-nav__link"
+              to="/"
+              title="Contacts"
+            />
+
+          </li>
+          <li className="footer-nav__item">
+            <NavigationLink
+              className="footer-nav__link"
+              to="/"
+              title="Rights"
+            />
+          </li>
+        </ul>
+      </nav>
+
+      <Link className="footer__link" to="/">
+        <div className="footer__back-link">
+          <span>Back to top</span>
+          <img className="footer-icon" src={BackToTop} />
+        </div>
+      </Link>
+    </div>
+  </footer>
 );
