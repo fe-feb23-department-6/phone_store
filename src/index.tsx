8e92694923d09a8cf11a0520f8d58dda1540db8e
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-06-12 13:42:13 +0300

    Added base context

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { StoreContextProvider } from './context/StoreContextProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -9,6 +9,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <BrowserRouter>
-    <App />
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>,
   </BrowserRouter>,
 );
---
+++ src/types/StoreContextData.ts
@@ -0,0 +1,6 @@
+import { PhoneCard } from './PhoneCard';
+
+export interface StoreContextData {
+  cartContents: PhoneCard[],
+  setCartContents: React.Dispatch<React.SetStateAction<PhoneCard[]>>;
+}
