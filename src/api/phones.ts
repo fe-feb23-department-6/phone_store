e7ef3dc97ee5c18606d36eb09ed3e6cd1dffe461
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-06-13 11:26:00 +0300

    fixed proper passing for search params

--- src/api/phones.ts
+++ src/api/phones.ts
@@ -1,13 +1,15 @@
-/* eslint-disable no-console */
 import { client } from '../utils/fetchClient';
-import { Phones } from '../Types/Phones';
+import { Phones } from '../types/Phones';
 
-export const getProducts = async(url: string) => {
-  const products = await client.getAllPhones<Phones[]>(url);
+// search params is equal to useLocation().search
+export const getProducts = async(searchParams: string) => {
+  const products = await client.getAllPhones<Phones[]>(
+    `products${searchParams}`,
+  );
 
-  products.map((item) => {
-    item.image = `https://backend-phone-store.onrender.com/${item.image}`;
-  });
+  //   products.map((item) => {
+  //     item.image = `https://backend-phone-store.onrender.com/${item.image}`;
+  //   });
 
   return products;
 };
--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const BASE_URL = 'https://backend-phone-store.onrender.com/';
 
-async function request<T>(url: string, method = 'GET'): Promise<T> {
+async function request<T>(searchParams: string, method = 'GET'): Promise<T> {
   const options: RequestInit = { method };
 
   try {
-    const response = await fetch(BASE_URL + url, {
+    const response = await fetch(BASE_URL + searchParams, {
       headers: {
         'Access-Control-Allow-Origin': 'http://localhost:3000',
       },
