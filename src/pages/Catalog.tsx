1709f28a5ec72c6c91d05d21e3c7f2921629f958
Mys Yaroslav <gotluck13@gmail.com>
2023-06-13 16:37:55 +0300

    Card and Catalog

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -3,7 +3,6 @@ import '../styles/fonts/fonts.scss';
 import '../styles/utils/_vars.scss';
 import '../styles/utils/_mixins.scss';
 
-import { CategoryName } from '../components/Catalog/CategoryName/CategoryName';
 import { SortBar } from '../components/Catalog/SortBar/SortBar';
 import { ProductsList } from '../components/Catalog/ProductsList/ProductsList';
 
@@ -11,7 +10,10 @@ import phonesFromServer from '../phonesBase.json';
 
 export const Catalog = () => (
   <div className="catalogContent">
-    <CategoryName productsQuantity={phonesFromServer.length} />
+    <div className="categoryName">
+      <h1 className="categoryName-text">Mobile phones</h1>
+      <p className="categoryName-quantity">{`${phonesFromServer.length} models`}</p>
+    </div>
     <SortBar />
     <ProductsList products={phonesFromServer} />
   </div>
