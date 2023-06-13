107caa1cc0832e22739ae73ed6ac8ec9495a8d19
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-06-13 10:39:27 +0300

    get all products from api

---
+++ src/Types/Phones.ts
@@ -0,0 +1,15 @@
+export interface Phones {
+  capacity: string;
+  category: string;
+  color: string;
+  fullPrice: number;
+  id: 'string';
+  image: string;
+  itemId: string;
+  name: string;
+  phoneId: string;
+  price: number;
+  ram: string;
+  screen: string;
+  year: number;
+}
