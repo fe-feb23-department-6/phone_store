63219dbab57000d12846822224f0e2f1e194f40f
Mys Yaroslav <gotluck13@gmail.com>
2023-06-14 20:32:02 +0300

    Cart item + cart layout

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import './PagesStyles/Cart.scss';
+import { CartList } from '../components/Cart/CartList/CartList';
+import { Checkout } from '../components/Cart/Checkout/Checkout';
+import ProductsInCart from './../phonesBase.json';
+
+const threeProductsInCart = ProductsInCart.slice(0, 3);
+
+// totalPrice = threeProductsInCart.reduce((x, sum) =>
+// (x.price * x.quantity) + sum, 0);
+// totalItems = threeProductsInCart.reduce((x, sum) =>
+// x.quantity + sum, 0);
 
 export const Cart = () => (
-  <div>
-    <h1>Cart</h1>
+  <div className="cart-wrapper">
+    <h1 className="cart-text">Cart</h1>
+    <div className="cart">
+      <CartList cartProducts={threeProductsInCart}/>
+      <Checkout totalPrice={2000} totalItems={3}/>
+    </div>
   </div>
 );
