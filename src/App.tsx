b2b606e795da3e4e733d9f9bd8ccbe8c0157a2e5
Alisa Vinnyk <alisavinnyk@gmail.com>
2023-06-16 12:02:32 +0100

    removed test

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.scss';
-// import { Home } from './pages/Home';
+import { Home } from './pages/Home';
 import { Catalog } from './pages/Catalog';
 import { Product } from './pages/Product';
 import { Favorites } from './pages/Favorites';
 import { Cart } from './pages/Cart';
-// import { NotFound } from './pages/NotFound';
+import { NotFound } from './pages/NotFound';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Contacts } from './pages/Contacts';
@@ -18,11 +18,10 @@ export const App = () => {
     <div className="App">
       <Header />
 
-      <main className="main">
-        <Product />
+      <main>
         <div>
           <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/" element={<Home />} />
             <Route path="/home" element={<Navigate to="/" replace />} />
             <Route path="/category/:categoryName">
               <Route index element={<Catalog />} />
@@ -32,7 +31,7 @@ export const App = () => {
             <Route path="/cart" element={<Cart />} />
             <Route path="/contacts" element={<Contacts />} />
             <Route path="/rights" element={<Rights />} />
-            {/* <Route path="*" element={<NotFound />} /> */}
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </main>
