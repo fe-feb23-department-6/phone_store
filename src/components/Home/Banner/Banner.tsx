6a45e636271d0e71fe18b01f04838bcc1e78f997
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-06-16 03:32:34 +0300

    banner on home page

--- src/components/Home/Banner/Banner.tsx
+++ src/components/Home/Banner/Banner.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -10,55 +10,55 @@ import './Banner.scss';
 
 export const Banner = () => {
   return (
-    <section className="banner">
+    <section className="banner-slider">
       <h2 className="banner-title">Welcome to Nice Gadgets store!</h2>
 
-      <Swiper
-        // install Swiper modules
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        slidesOffsetAfter={10000}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        loop={true}
-        autoplay={{
-          delay: 5,
-        }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-        speed={600}
-      >
-        <SwiperSlide>
-          <div className="banner-item">
+      <div className="banner">
+        <button className="banner-control banner-control__prev"></button>
+        <button className="banner-control banner-control__next"></button>
+        <Swiper
+          modules={[Autoplay, Navigation, Pagination]}
+          slidesOffsetAfter={10000}
+          slidesPerView={1}
+          navigation={{
+            prevEl: '.banner-control__prev',
+            nextEl: '.banner-control__next',
+          }}
+          pagination={{ el: '.swiper-pag', clickable: true }}
+          loop
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: true,
+          }}
+        >
+          <SwiperSlide>
             <img
-              className="banner-item__img"
               // eslint-disable-next-line max-len
               src="https://backend-phone-store.onrender.com/img/banner-phones.png"
               alt="iphone-banner"
             />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="banner-item">
+          </SwiperSlide>
+          <SwiperSlide>
             <img
-              className="banner-item__img"
               // eslint-disable-next-line max-len
               src="https://backend-phone-store.onrender.com/img/banner-phones.png"
               alt="iphone-banner"
             />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="banner-item">
+          </SwiperSlide>
+          <SwiperSlide>
             <img
-              className="banner-item__img"
               // eslint-disable-next-line max-len
               src="https://backend-phone-store.onrender.com/img/banner-phones.png"
               alt="iphone-banner"
             />
-          </div>
-        </SwiperSlide>
-      </Swiper>
+          </SwiperSlide>
+        </Swiper>
+        <div className="swiper-pag">
+          <div className="swiper-pag__bullet" />
+          <div className="swiper-pag__bullet" />
+          <div className="swiper-pag__bullet" />
+        </div>
+      </div>
     </section>
   );
 };
