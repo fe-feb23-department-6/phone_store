381812e075faf520731b4bf8936a80b095b1d2f7
Mys Yaroslav <gotluck13@gmail.com>
2023-06-17 15:31:15 +0300

    Cart Modal

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import './PagesStyles/Cart.scss';
 import { Checkout } from '../components/Cart/Checkout/Checkout';
 import ProductsInCart from './../phonesBase.json';
 import { CartList } from '../components/Cart/Cartlist/CartList';
+import { CartModal } from '../components/Cart/CartModal/Cartmodal';
 
 const threeProductsInCart = ProductsInCart.slice(0, 3);
 
@@ -10,12 +12,32 @@ const threeProductsInCart = ProductsInCart.slice(0, 3);
 // totalItems = threeProductsInCart.reduce((x, sum) =>
 // x.quantity + sum, 0);
 
-export const Cart = () => (
-  <div className="cart-wrapper">
-    <h1 className="cart-text">Cart</h1>
-    <div className="cart">
-      <CartList cartProducts={threeProductsInCart} />
-      <Checkout totalPrice={2000} totalItems={3} />
-    </div>
-  </div>
-);
+export const Cart = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const onCheckout = () => {
+    setTimeout(() => {
+      setShowModal(true);
+    }, 100);
+  };
+
+  return (
+    <>
+      {showModal ? (
+        <CartModal />
+      ) : (
+        <div className="cart-wrapper">
+          <h1 className="cart-text">Cart</h1>
+          <div className="cart">
+            <CartList cartProducts={threeProductsInCart} />
+            <Checkout
+              totalPrice={2000}
+              totalItems={3}
+              onCheckout={onCheckout}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
