6670738f089039352f40d3f7003facdd49098515
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-06-18 16:29:18 +0300

    fixed links from burger menu

--- src/components/ActionLinks/ActionLinks.tsx
+++ src/components/ActionLinks/ActionLinks.tsx
@@ -7,14 +7,14 @@ import Basket from '../../img/icons/shopping_bag.svg';
 export const ActionsLink = () => (
   <div className="header__actions">
     <NavLink
-      to={'/favorites'}
+      to="/favorites"
       className={({ isActive }) => cn('action', { 'is-active': isActive })}
     >
       <img src={Favorite} alt="favorites" className="action__image" />
     </NavLink>
 
     <NavLink
-      to={'/cart'}
+      to="/cart"
       className={({ isActive }) => cn('action', { 'is-active': isActive })}
     >
       <img src={Basket} alt="cart" className="action__image" />
