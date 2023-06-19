741c2431c9e806b18169189d37238284c72cb464
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-06-19 15:55:27 +0300

    added GoHomeButton to favorites page

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import { GoHomeButton } from '../components/GoHomeButton';
+import './PagesStyles/Favorites.scss';
 
 export const Favorites = () => (
-  <div>
-    <h1>Favorites</h1>
+  <div className='favorites'>
+    <GoHomeButton />
+    <h1 className='favorites__title'>Favorites</h1>
   </div>
 );
