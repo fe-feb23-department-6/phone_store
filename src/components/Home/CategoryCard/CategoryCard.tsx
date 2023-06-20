835780579d6bede2269007c2efadf954b6fc6ae0
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-06-20 17:39:06 +0300

    added amount too categories

--- src/components/Home/CategoryCard/CategoryCard.tsx
+++ src/components/Home/CategoryCard/CategoryCard.tsx
@@ -1,5 +1,6 @@
-import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { FC, useCallback, useEffect, useState } from 'react';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
+import { getProducts } from '../../../api/phones';
 
 import './Category.scss';
 
@@ -10,6 +11,27 @@ interface Props {
 }
 
 export const CategoryCard: FC<Props> = ({ image, path, categoryName }) => {
+  const [productsCount, setProductsCount] = useState<null | number>(null);
+  const [searchParams] = useSearchParams();
+  const paramsString = useLocation().search;
+
+  const getCatalogContents = useCallback(async() => {
+    try {
+      const productsData = await getProducts(paramsString);
+      const {
+        totalCount: productsQuantity,
+      } = productsData;
+
+      setProductsCount(productsQuantity);
+    } catch {
+      throw new Error('Server error');
+    }
+  }, [searchParams]);
+
+  useEffect(() => {
+    getCatalogContents();
+  }, [searchParams]);
+
   return (
     <div className="category__item">
       <Link to={path} className="category__item__photo">
@@ -20,7 +42,7 @@ export const CategoryCard: FC<Props> = ({ image, path, categoryName }) => {
         {categoryName}
       </Link>
 
-      <div className="category__item__model">95 models</div>
+      <div className="category__item__model">{productsCount} models</div>
     </div>
   );
 };
