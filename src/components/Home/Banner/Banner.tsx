27f7d3493b7a9e852e89ba3ac795951e1ebdddc5
Alisa Vinnyk <alisavinnyk@gmail.com>
2023-06-20 17:13:53 +0100

    banner layout

--- src/components/Home/Banner/Banner.tsx
+++ src/components/Home/Banner/Banner.tsx
@@ -7,9 +7,10 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import './Banner.scss';
-import iphoneBanner from './img/iphone-banner.jpg';
-import ipadBanner from './img/ipad-banner.jpg';
-import watchBanner from './img/watch-banner.png';
+// import iphoneBanner from './img/iphone-banner.jpg';
+// import ipadBanner from './img/ipad-banner.jpg';
+// import watchBanner from './img/watch-banner.png';
+import { BannerSlide } from '../../BannerSlide';
 
 export const Banner = () => {
   return (
@@ -36,6 +37,27 @@ export const Banner = () => {
           }}
         >
           <SwiperSlide>
+            <BannerSlide
+              productTitle={'Iphone 11'}
+              productQuote={'Lots more power. Lots more battery life.'}
+              photoIndex={1}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <BannerSlide
+              productTitle={'Iphone 11 pro'}
+              productQuote={'Pro. Beyond.'}
+              photoIndex={2}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <BannerSlide
+              productTitle={'Iphone 11 pro max'}
+              productQuote={'Pro cameras. Pro display. Pro performance.'}
+              photoIndex={3}
+            />
+          </SwiperSlide>
+          {/* <SwiperSlide>
             <img
               className="banner-img"
               // eslint-disable-next-line max-len
@@ -58,7 +80,7 @@ export const Banner = () => {
               src={watchBanner}
               alt="iphone-banner"
             />
-          </SwiperSlide>
+          </SwiperSlide> */}
         </Swiper>
         <div className="swiper-pag">
           <div className="swiper-pag__bullet" />
