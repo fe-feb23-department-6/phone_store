7d4582464301463c9e8b17f9da50acc2fb8623f2
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-06-21 12:50:43 +0300

    fixing not found after added breadcrumbs

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -10,19 +10,16 @@ export const NotFound = () => (
       <h1 className="not-found__title">404</h1>
 
       <div className="not-found__content">
-        <p className="not-found__message">
-          Something's missing.
-        </p>
+        <p className="not-found__message">Something's missing.</p>
         <p className="not-found__message">
           This Page doesn't exist or was removed.
         </p>
-        <p className="not-found__message">
-          We suggest you go back to home.
-        </p>
+        <p className="not-found__message">We suggest you go back to home.</p>
 
         <Link className="not-found__button" to="/">
           Go to Home
         </Link>
       </div>
-    </div></>
+    </div>
+  </>
 );
