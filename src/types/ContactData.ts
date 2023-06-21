2b5c5bf98833d9868f8e0cf19b38172efabfa43f
Alisa Vinnyk <alisavinnyk@gmail.com>
2023-06-21 01:13:41 +0100

    added types to contact

---
+++ src/types/ContactData.ts
@@ -0,0 +1,11 @@
+import { SocialMediaData } from './SocialMediaData';
+
+export interface ContactData {
+  personName: string;
+  personId: string;
+  gmail: string;
+  linkedin: string;
+  github: string;
+  telegram: string;
+  socialMedia: SocialMediaData[];
+}
