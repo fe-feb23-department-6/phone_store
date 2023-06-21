9d091e898a8ed2f5c2bdabb1200183a7101f29db
Mys Yaroslav <gotluck13@gmail.com>
2023-06-21 14:00:52 +0300

    Authentification v3

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.scss';
 import { Home } from './pages/Home';
 import { Catalog } from './pages/Catalog';
 import { Product } from './pages/Product';
 import { Favorites } from './pages/Favorites';
 import { Cart } from './pages/Cart';
 import { NotFound } from './pages/NotFound';
-import { Contacts } from './pages/Contacts';
-import { Rights } from './pages/Rights';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
-import { ThreadData } from './types/ThreadData';
-import 'bulma/css/bulma.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-import './App.scss';
+import { Contacts } from './pages/Contacts';
+import { Rights } from './pages/Rights';
+import { LoginPage } from './pages/LoginPage';
+import { SignupPage } from './pages/SignupPage';
 
 export const App = () => {
   return (
@@ -20,27 +21,28 @@ export const App = () => {
       <Header />
 
       <main className="main">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Navigate to="/" replace />} />
-          <Route
-            path="/category/:categoryName"
-            handle={{
-              crumb: (data: ThreadData) => <span>{data.threadName}</span>,
-            }}
-          >
-            <Route index element={<Catalog />} />
-            <Route path=":prodId" element={<Product />} />
-          </Route>
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/rights" element={<Rights />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Navigate to="/" replace />} />
+            <Route path="/category/:categoryName">
+              <Route index element={<Catalog />} />
+              <Route path=":productId" element={<Product />} />
+            </Route>
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/rights" element={<Rights />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
       </main>
 
-      <Footer />
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
